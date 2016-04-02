512f22b576e2267e87c41b3a63cbbf43414f373b
mtrop <mtropiano@gmail.com>
2016-04-02 04:07:46 -0400

    Stuff.

---
+++ testsrc/net/mtrop/tame/CommandDump.java
@@ -0,0 +1,22 @@
+package net.mtrop.tame;
+
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public final class CommandDump 
+{
+
+	public static void main(String[] args)
+	{
+		PrintStream out = System.out;
+		
+		for (TAMECommand command : TAMECommand.values())
+		{
+			if (command.isInternal() || command.isLanguage())
+				continue;
+			
+			out.println(command.name() + " Returns: " + command.getReturnType() + " Arguments: " + Arrays.toString(command.getArgumentTypes()));
+		}
+	}
+
+}
