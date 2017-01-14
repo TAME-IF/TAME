bfe9844b88fabb92c35487d108e71be569389f40
mtrop <mtropiano@gmail.com>
2017-01-13 21:30:27 -0500

    Forgot a file.

---
+++ src/net/mtrop/tame/element/ObjectContainer.java
@@ -0,0 +1,11 @@
+package net.mtrop.tame.element;
+
+/**
+ * Inherited by elements that hold objects.
+ * This interface is purely informational.
+ * @author Matthew Tropiano
+ */
+public interface ObjectContainer 
+{
+
+}
