f32e488a2e04577516655e496387b107961350b6
mtrop <mtropiano@gmail.com>
2017-02-06 17:02:00 -0500

    Dox changes.

--- utilsrc/net/mtrop/tame/TAMEDoxCommandDump.java
+++ utilsrc/net/mtrop/tame/TAMEDoxCommandDump.java
@@ -16,36 +16,45 @@ import net.mtrop.tame.lang.ArgumentType;
 public final class TAMEDoxCommandDump 
 {
 
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		PrintStream out = System.out;
+		PrintStream out = new PrintStream(System.out, true, "UTF-8");
+		
+		out.println("<?xml encoding=\"UTF-8\"?>");
+		out.println();
+
+		out.println("<!-- TEMPLATES -->");
+		out.println("<!-- <tamedox:commandreturntype type=\"VALUE TYPE HERE\">REASON GOES HERE</tamedox:commandreturntype> -->");
+		out.println();
 		
 		for (TAMECommand command : TAMECommand.values())
 		{
 			if (command.isInternal())
 				continue;
-			
-			out.println("<tamedox:command>");
-			out.println("\t<tamedox:commandname>"+command.name()+"</tamedox:commandname>");
+
+			out.println("<!-- Command: "+command.name()+" -->");
+			out.println("<tamedox:command name=\""+command.name()+"\" group=\"UNNAMED\" sort=\"0\"/>");
 			if (command.getArgumentTypes() != null)
 			{
 				out.println("\t<tamedox:commandargs>");
 				for (ArgumentType at : command.getArgumentTypes())
-					out.println("\t\t<tamedox:commandarg>"+at.name()+"</tamedox:commandarg>");
+					out.println("\t\t<tamedox:commandarg type=\""+at.name()+"\"><!-- PURPOSE GOES HERE --></tamedox:commandarg>");
 				out.println("\t</tamedox:commandargs>");
-				out.println("\t<tamedox:commandreturn>"+command.getReturnType()+"</tamedox:commandreturn>");
-
-				out.println("\t<tamedox:commanddocs>");
-				out.println("\t\t<!--ADD SHIT HERE -->");
-				out.println("\t</tamedox:commanddocs>");
-				out.println("\t<tamedox:commandexample script=\"scripts/examples/command-"+command.name().toLowerCase()+".tame\"/>");
-				out.println("\t<tamedox:commandtechnoweenie>");
-				out.println("\t\t<!--ADD SHIT HERE -->");
-				out.println("\t</tamedox:commandtechnoweenie>");
+				if (command.getReturnType() != null)
+				{
+					out.println("\t<tamedox:commandreturntypes>");
+					out.println("\t\t<!--ADD SHIT HERE -->");
+					out.println("\t</tamedox:commandreturntypes>");
+				}
+				out.println("\t<tamedox:commanddocs src=\"static/command-docs-"+command.name().toLowerCase()+".html\"/>");
+				out.println("\t<tamedox:commandexample script=\"scripts/examples/command-"+command.name().toLowerCase()+"\"/>");
+				out.println("\t<tamedox:commandtech src=\"static/command-docs-"+command.name().toLowerCase()+"-tech.html\"/>");
 			}
 			out.println("</tamedox:command>");
+			out.println();
 		}
-
+	
+		out.close();
 	}
 
 }
