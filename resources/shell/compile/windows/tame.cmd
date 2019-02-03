26b362b8df29dfa1a0dc4bdfe0d5ac411b87331e
mtrop <mtropiano@gmail.com>
2019-02-03 00:04:46 -0500

    Added windows CMD scripts

---
+++ resources/shell/compile/windows/tame.cmd
@@ -0,0 +1,1 @@
+@java -cp "%~dp0\{{COMPILE_CLASSPATH}}" com.tameif.tame.console.TAMEConsoleShellMain %*
---
+++ resources/shell/jre/windows/tamec.cmd
@@ -0,0 +1,1 @@
+@"%~dp0\..\jre\bin\java.exe" -cp "%~dp0\..\jar\{{JAR_ONE_FILENAME}}" com.tameif.tame.compiler.TAMECompilerMain %*
